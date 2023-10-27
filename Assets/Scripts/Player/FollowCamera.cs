18cd4323ffc00494dbcc68ac8f3cf16d44aecf5d
aeju <aejuchoe@naver.com>
2023-10-27 23:43:46 +0900

    Modify: FollowCam
    제대로 구현 완료

---
+++ Assets/Scripts/Player/FollowCamera.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+// 기존 FollowCam 문제점 : 실행되면, 기존 카메라 위치/각도 유지 x
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target; 
+
+    private Vector3 offset; 
+    private Quaternion initialRotation; 
+    
+    void Start()
+    {
+        // initial offset : 카메라 - 플레이어 위치
+        offset = transform.position - target.position;
+
+        // initial rotation 
+        initialRotation = transform.rotation;
+    }
+
+    void LateUpdate()
+    {
+        // 플레이어 위치 + offset
+        Vector3 newPos = target.position + offset;
+        // 카메라의 위치 = newPos로 업데이트
+        transform.position = newPos;
+        
+        transform.rotation = initialRotation;
+    }
+}
