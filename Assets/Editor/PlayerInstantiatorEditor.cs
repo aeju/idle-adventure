8704a06f8f179bb55b5ac8271582e74b96c3307c
aeju <aejuchoe@naver.com>
2023-12-04 20:03:09 +0900

    Squashed commit of the following:
    commit ae57ae2c1c8504c646f43473c890bbb1cd0f6f9d
    Author: aeju <aejuchoe@naver.com>
    Date:   Mon Dec 4 20:02:25 2023 +0900
    
        [editor]추가: add component - sprite renderer (자식 오브젝트에)
    
    commit c136b3921e6f42d3e1aa9f7a4a16f2ddb30b4820
    Author: aeju <aejuchoe@naver.com>
    Date:   Mon Dec 4 19:35:47 2023 +0900
    
        [editor]추가: 캔버스 프리팹 (슬라이더 포함) 등록
    
    commit 3eec7a0cbe4030d27364ebbdebad9ed24d15d92d
    Author: aeju <aejuchoe@naver.com>
    Date:   Mon Dec 4 18:56:44 2023 +0900
    
        [editor]추가: playerstats, playercontroller 자동으로 등록
    
    commit bc0c9377b4440a59b25dfa53cf5ea57aca91a544
    Author: aeju <aejuchoe@naver.com>
    Date:   Mon Dec 4 17:32:48 2023 +0900
    
        [editor]수정: 화면에 저장 x, 프리팹으로 저장

--- Assets/Editor/PlayerInstantiatorEditor.cs
+++ Assets/Editor/PlayerInstantiatorEditor.cs
@@ -2,30 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.PackageManager.UI;
+
 
 // 플레이어 프리팹, 스크립트, 저장할 위치 -> 부모 게임 오브젝트
+// 플레이어 프리팹 - 스크립트2, 캔버스 프리팹(hp, 쿨타임 슬라이더), 스프라이트 렌더러
+
 // 지정할 것 : 플레이어 포지션, 스케일
+// script1 = PlayerStats.cs
+// script2 = PlayerController.cs
 public class PlayerInstantiatorEditor : EditorWindow
 {
-    private GameObject prefab;
-    private string objectName = "NewObject";
+    // 설정할 필요 o
+    private string objectName = "PlayerName";
+    private GameObject PlayerPrefab;
+    private GameObject CanvasPrefab;
     private string savePath = "Assets/03. Prefabs/";
-    private Vector3 position = Vector3.zero;
-    private Vector3 scale = Vector3.one;
+    
+    // 설정할 필요 x
+    private MonoScript script1;
+    private MonoScript script2;
+    private Vector3 position = new Vector3(0, 0.75f, 0);
+    private Vector3 scale = new Vector3(2, 2, -1);
 
     [MenuItem("Tools/Prefab Instantiator")]
     public static void ShowWindow()
     {
-        GetWindow<PlayerInstantiatorEditor>("Player Instantiator");
+        var window = GetWindow<PlayerInstantiatorEditor>("Player Instantiator");
+        window.AutoScripts();
     }
 
     private void OnGUI()
     {
         GUILayout.Label("Prefab Instantiator", EditorStyles.boldLabel);
 
-        prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), false);
         objectName = EditorGUILayout.TextField("Object Name", objectName);
+        PlayerPrefab = (GameObject)EditorGUILayout.ObjectField("Player Prefab", PlayerPrefab, typeof(GameObject), false);
+        CanvasPrefab = (GameObject)EditorGUILayout.ObjectField("Canvas Prefab", CanvasPrefab, typeof(GameObject), false);
         savePath = EditorGUILayout.TextField("Save Path", savePath);
+        
+        script1 = (MonoScript)EditorGUILayout.ObjectField("Player Stats Script", script1, typeof(MonoScript), false);
+        script2 = (MonoScript)EditorGUILayout.ObjectField("Player Controller Script", script2, typeof(MonoScript), false);
         position = EditorGUILayout.Vector3Field("Position", position);
         scale = EditorGUILayout.Vector3Field("Scale", scale);
         
@@ -35,18 +52,51 @@ public class PlayerInstantiatorEditor : EditorWindow
         }
     }
 
+    private void AutoScripts()
+    {
+        script1 = FindScriptByName("PlayerStats");
+        script2 = FindScriptByName("PlayerController");
+    }
+
+    private MonoScript FindScriptByName(string scriptName)
+    {
+        foreach (var script in Resources.FindObjectsOfTypeAll<MonoScript>())
+        {
+            if (script.name == scriptName)
+                return script;
+        }
+
+        return null;
+    }
+
+
     private void CreatePrefabInstance()
     {
-        if (prefab == null)
+        if (PlayerPrefab == null || CanvasPrefab == null)
         {
-            EditorUtility.DisplayDialog("Error", "No prefab selected", "OK");
+            EditorUtility.DisplayDialog("Error", "빈 프리팹 있음!", "OK");
             return;
         }
 
         GameObject instance = new GameObject(objectName);
-        GameObject prefabInstance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+        GameObject prefabInstance = PrefabUtility.InstantiatePrefab(PlayerPrefab) as GameObject;
+        prefabInstance.transform.SetParent(instance.transform);
         prefabInstance.transform.localPosition = position;
         prefabInstance.transform.localScale = scale;
+        
+        GameObject canvasInstance = PrefabUtility.InstantiatePrefab(CanvasPrefab) as GameObject;
+        canvasInstance.transform.SetParent(prefabInstance.transform, false); // 캔버스 프리팹(Slider) - 플레이어 프리팹 자식으로 설정
+        prefabInstance.AddComponent<SpriteRenderer>(); // 스프라이트 렌더러 컴포넌트 추가 
+        
+        if (script1 != null)
+        {
+            prefabInstance.AddComponent(script1.GetClass());
+        }
+
+        if (script2 != null)
+        {
+            prefabInstance.AddComponent(script2.GetClass());
+        }
 
         string finalPath = AssetDatabase.GenerateUniqueAssetPath(savePath + "/" + objectName + ".prefab");
         PrefabUtility.SaveAsPrefabAsset(instance, finalPath);
