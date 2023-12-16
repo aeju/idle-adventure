76c9f0ec2360d54fcca4d99c98b7cc391a17fddf
aeju <aejuchoe@naver.com>
2023-12-16 23:41:06 +0900

    [attack]추가: 터치 -> 조이스틱(드래그 ㅐ)

--- Assets/02. Scripts/FullScreenJoyStick.cs
+++ Assets/02. Scripts/FullScreenJoyStick.cs
@@ -1,39 +1,60 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// 터치 : bg 나타남 -> 터치 해제 : 사라짐
+// 드래그 : handle 
 public class FullScreenJoystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     public RectTransform joystickBackground;
     public RectTransform joystickHandle;
-
-    public void Start()
-    {
-        Debug.Log("joystick start");
-    }
+    
+    public bool isDragging = false; // 드래그되는 동안만 입력값 받기
     
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("joystick down");
-        joystickBackground.position = eventData.position;
+        // 조이스틱 위치 = 터치 위치 
+        joystickBackground.position = eventData.position; // Vector3로 받아와서 x
         joystickHandle.position = eventData.position;
-        joystickBackground.gameObject.SetActive(true);
-        joystickHandle.gameObject.SetActive(true);
+        
+        isDragging = true;
+        JoystickOn();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("joystick drag");
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBackground.parent as RectTransform, eventData.position, null, out localPoint);
+        
+        // 드래그 방향 : 현재 위치 - 배경 위치 
         Vector2 direction = eventData.position - (Vector2)joystickBackground.position;
-        //float radius = joystickBackground.sizeDelta.x / 2; // 절반으로 제한
-        float radius = joystickBackground.sizeDelta.x / 4;
-        Vector3 newHandlePosition = (Vector3)Vector2.ClampMagnitude(direction, radius);
-        joystickHandle.position = joystickBackground.position + newHandlePosition;
+        // 핸들 반경 제한
+        float radius = joystickBackground.sizeDelta.x / 2;
+        Vector2 clampedDirection = Vector2.ClampMagnitude(direction, radius);
+        joystickHandle.anchoredPosition = clampedDirection;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         Debug.Log("joystick up");
+        isDragging = false;
+        JoystickOff();
+    }
+
+    public void JoystickOn()
+    {
+        joystickBackground.gameObject.SetActive(true);
+        joystickHandle.gameObject.SetActive(true);
+    }
+
+    public void JoystickOff()
+    {
         joystickBackground.gameObject.SetActive(false);
         joystickHandle.gameObject.SetActive(false);
+    }
+
+    public Vector2 GetInputDirection()
+    {
+        return (joystickHandle.anchoredPosition - joystickBackground.anchoredPosition).normalized;
     }
 }
