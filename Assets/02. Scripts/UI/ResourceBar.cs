ff827cd85f7d2f4e5be3af4a2e1280b2474d0f8f
aeju <aejuchoe@naver.com>
2023-12-21 00:26:44 +0900

    [mainUI]수정: 코인 단위 표기

--- Assets/02. Scripts/UI/ResourceBar.cs
+++ Assets/02. Scripts/UI/ResourceBar.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+// 루비, 코인(단위!) / 뽑기 
+// 전투력 
+// 드랍 아이템 -> 리소스바 UI 반영 
 public class ResourceBar : MonoBehaviour
 {
     protected ResourceManager resoureInfo;
@@ -47,7 +50,29 @@ public class ResourceBar : MonoBehaviour
             summon_Ticket = resoureInfo.current_summon_Ticket;
 
             rubyText.text = ruby.ToString();
-            coinText.text = coin.ToString();
+            //coinText.text = coin.ToString();
+            coinText.text = FormatCoinUnit(coin);
+        }
+    }
+
+    // K : 1,000
+    // M : 1,000,000
+    // B : 1,000,000,000
+    string FormatCoinUnit(int number)
+    {
+        if (number >= 1000)
+        {
+            return (number / 1000f).ToString("0.#") + "K";
+        }
+        
+        else if (number >= 1000000)
+        {
+            return (number / 1000000f).ToString("0.#") + "M";
+        }
+        
+        else
+        {
+            return number.ToString();
         }
     }
 }
