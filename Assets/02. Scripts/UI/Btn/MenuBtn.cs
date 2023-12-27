6d05499ebf9517f360740eb90137c60aefa548b5
aeju <aejuchoe@naver.com>
2023-12-27 21:00:02 +0900

    [Settings]추가: 메뉴 버튼 클릭 -> 메뉴 패널 활성화 / 비활성화(닫기)
    비활성화 조건 : X btn , 메뉴 패널 밖 터치

---
+++ Assets/02. Scripts/UI/Btn/MenuBtn.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+
+// 처음 : menuPanel 비활성화
+// 메뉴 아이콘 클릭 : menuPanel 활성화
+// menuPanel이 활성화된 상태에서, Xbtn 터치하거나 그 외 영역 터치 -> menuPanel 비활성화 
+public class MenuBtn : MonoBehaviour
+{
+    public Button menuBtn; // 메뉴 버튼
+    public Button closeBtn; // 클로즈 버튼 
+    
+    public GameObject menuPanel; // 메뉴 패널 
+    
+    void Start()
+    {
+        menuPanel.SetActive(false);
+        
+        menuBtn.OnClickAsObservable().Subscribe(_ =>
+        {
+            menuPanel.SetActive(true);
+        }).AddTo(this);
+        
+        closeBtn.OnClickAsObservable().Subscribe(_ =>
+        {
+            menuPanel.SetActive(false);
+        }).AddTo(this);
+        
+        // 터치 감지 
+        this.UpdateAsObservable()
+            .Where(_ => menuPanel.activeSelf) // 메뉴 패널이 활성화된 상황에서
+            .Where(_ => Input.GetMouseButtonDown(0)) // 터치가 입력될 때만 구독 내부 코드 실행 
+            .Subscribe(_ =>
+            {
+                // 터치가 메뉴 패널 밖에 있는 경우, 메뉴 패널을 비활성화 
+                if (!RectTransformUtility.RectangleContainsScreenPoint( // 터치 -> 메뉴 패널의 RectTransform 범위 내에 있는지 확인 
+                        menuPanel.GetComponent<RectTransform>(), 
+                        Input.mousePosition, // 터치 현재 위치
+                        Camera.main)) // 스크린 좌표 -> 월드 좌표로 전환 
+                {
+                    menuPanel.SetActive(false); // 비활성화 
+                }
+            }).AddTo(this);
+    }
+}
