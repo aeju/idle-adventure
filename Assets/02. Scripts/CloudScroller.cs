9dc1c553fc015eb9e8fa2ed12d6f0615c2933d7e
aeju <aejuchoe@naver.com>
2023-12-28 02:07:36 +0900

    [attack]수정: 구름 y축 움직임 추가

--- Assets/02. Scripts/CloudScroller.cs
+++ Assets/02. Scripts/CloudScroller.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 // 오른쪽으로 스크롤
 // 가장 오른쪽 레이어(레이어6, 마지막 레이어) -> 레이어 width만큼 이동
 // -> 첫번째 레이어(가장 왼쪽)로 이동 (좌표: 첫번째 레이어 - width)
+// 구름 y축으로 움직임 추가 -> 사인파 사용 
 public class CloudScroller : MonoBehaviour
 {
     public float scrollSpeed = 0.5f; // 스크롤 속도
     private float originalLastPositionX; // 마지막 레이어의 원래 x좌표
     public float layerSpacing = 38.0f; // 레이어 간 간격 (레이어 너비)
+    
+    public float verticalAmplitude = 0.01f; // 수직 이동의 진폭
+    public float verticalFrequency = 1.0f; // 수직 이동의 주파수
 
     void Start()
     {
@@ -29,6 +33,10 @@ public class CloudScroller : MonoBehaviour
         {
             Transform cloud = transform.GetChild(i);
             cloud.position += Vector3.right * scrollSpeed * Time.deltaTime; // 구름 위치 = 오른쪽 * 스크롤 속도 * 시간 
+            
+            // 수직 이동 -> 사인파 사용
+            float sineValue = Mathf.Sin(Time.time * verticalFrequency + i) * verticalAmplitude; // i = 위상 오프셋 
+            cloud.position = new Vector3(cloud.position.x, cloud.position.y + sineValue, cloud.position.z);
             
             // 마지막 레이어 구름이 원래 위치에서 레이어 너비만큼 움직였는지 확인 
             if (lastChild.position.x >= originalLastPositionX + layerSpacing)
