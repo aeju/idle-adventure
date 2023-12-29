ec8335c46ada5e3d86568aa87bbfca29464cfd95
aeju <aejuchoe@naver.com>
2023-12-30 03:22:27 +0900

    [attack]수정: 재화 정보 단위 축약 / 추가 : 전리품 UI
    commit 3a3d954797e803d5c62899a71698fce125c547e6
    Author: aeju <aejuchoe@naver.com>
    Date:   Sat Dec 30 03:18:46 2023 +0900
    
        [attack]수정: 재화 정보 축약 (B -> G 수정 / 이후 단위도 추가)
    
    commit ebe98f5211e4b6cfd9a780f08dc26bd84769f742
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Dec 28 22:24:44 2023 +0900
    
        [attack]수정: fov, item 버튼 위치 교체
    
        - 획득 아이템 UI : 기본 -> 열린 상태
    
    commit 94c754e58ebcfa223bd9a1d89f898fc66195ef01
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Dec 28 22:16:40 2023 +0900
    
        [attack]추가: 획득 아이템 UI 배치
    
        - 사냥부터 끝내고 작업
    
    commit 9dc1c553fc015eb9e8fa2ed12d6f0615c2933d7e
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Dec 28 02:07:36 2023 +0900
    
        [attack]수정: 구름 y축 움직임 추가
    
    commit ca94c30e7bfadf10fe3f01519da5e50f875d72bc
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Dec 28 01:30:28 2023 +0900
    
        [attack]추가: 구름 레이어 무한루프 (오른쪽 방향)
    
    commit 5f4bef283b13363d0c73906b97ce55d493474723
    Author: aeju <aejuchoe@naver.com>
    Date:   Wed Dec 27 23:37:25 2023 +0900
    
        [attack]수정: develpScene 복사 -> attackScene 교체

--- Assets/02. Scripts/UI/ResourceBar.cs
+++ Assets/02. Scripts/UI/ResourceBar.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-// 루비, 코인(단위!) / 뽑기 
+// 루비, 코인(단위!) / 뽑기 = 카드 
+// 코인 단위 축약 : 4자리 미만 -> 축약할 필요 x
 // 전투력 
 // 드랍 아이템 -> 리소스바 UI 반영 
 public class ResourceBar : MonoBehaviour
@@ -55,24 +56,50 @@ public class ResourceBar : MonoBehaviour
         }
     }
 
-    // K : 1,000
-    // M : 1,000,000
-    // B : 1,000,000,000
+    // K(Kilo 킬로) : 1,000
+    // M(Mega 메가) : 1,000,000
+    // G(Giga 기가) : 1,000,000,000
+    // T(Tera 테라) : 1,000,000,000,000
+    // P(Peta 페타) : 1,000,000,000,000,000
+    // E(Exa 엑사) : 1,000,000,000,000,000,000
+    // Z(Zetta 제타) : 1,000,000,000,000,000,000,000
+    // Y(Yotta 요타) : 1,000,000,000,000,000,000,000,000
+    // 각 단위 : 1000의 거듭제곱 
     string FormatCoinUnit(int number)
     {
-        if (number >= 1000)
+        if (number >= 1000) 
         {
-            return (number / 1000f).ToString("0.#") + "K";
+            return (number / 1000f).ToString("0.#") + "K"; // 킬로
         }
         
         else if (number >= 1000000)
         {
-            return (number / 1000000f).ToString("0.#") + "M";
+            return (number / 1000000f).ToString("0.#") + "M"; // 메가
         }
+
+        else if (number >= 1000000000)
+        {
+            return (number / 1000000000f).ToString("0.#") + "G"; // 기가
+        }
+
+        else if (number >= 1000000000000) 
+        {
+            return (number / 1000000000000f).ToString("0.#") + "T"; // 테라
+        }
+        
+        else if (number >= 1000000000000000) 
+        {
+            return (number / 1000000000000000f).ToString("0.#") + "P"; // 페타
+        }
+        
+        else if (number >= 1000000000000000000) 
+        {
+            return (number / 1000000000000000000f).ToString("0.#") + "E"; // 엑사
+        }        
         
         else
         {
-            return number.ToString();
+            return number.ToString(); // 1000이하 : 축약할 필요 x
         }
     }
 }
