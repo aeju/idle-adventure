815722259f031221d00725d6627859ed426c3a60
aeju <aejuchoe@naver.com>
2024-01-03 00:55:36 +0900

    [idleMode]추가: 배터리 상태 이미지 표시
    - 배터리 반영 확인 o
    - 수정 필요 : 충전 -> 충전 이미지로 변경

--- Assets/02. Scripts/UI/BatteryStatus.cs
+++ Assets/02. Scripts/UI/BatteryStatus.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-// 배터리 퍼센트
 // 배터리 상태 : ~10, ~25, ~50, ~75, ~100 / charging
 public class BatteryStatus : MonoBehaviour
 {
-    public TextMeshProUGUI batteryLevelText;
-
     public Image batteryImage;
     public Sprite[] batterySprites;
     public Sprite chargingSprite;
@@ -21,22 +18,16 @@ public class BatteryStatus : MonoBehaviour
 
     void CheckBatteryStatus()
     {
-        // 퍼센트 
         float batteryValue = SystemInfo.batteryLevel; // Return 0 ~ 1
         int batteryPercentage = (int)(batteryValue * 100);
         
-        if (batteryLevelText != null)
-            batteryLevelText.text = "Battery Level: " + batteryPercentage + "%";
-        
-        // 이미지 
         UnityEngine.BatteryStatus batteryStatus = SystemInfo.batteryStatus;
         
-        // 충전 중인지 확인 
         if (batteryStatus == UnityEngine.BatteryStatus.Charging)
         {
             SetBatterySprite(chargingSprite);
         }
-        else // 충전 중이 아니면, 배터리 상태 이미지 표시 
+        else
         {
             int spriteIndex = GetBatterySpriteIndex(batteryPercentage);
             if (spriteIndex >= 0 && spriteIndex < batterySprites.Length)
@@ -57,14 +48,14 @@ public class BatteryStatus : MonoBehaviour
     int GetBatterySpriteIndex(int batteryPercentage)
     {
         if (batteryPercentage <= 10)
-            return 0; // 0-10%
+            return 0;
         else if (batteryPercentage <= 25)
-            return 1; // 11-25%
+            return 1;
         else if (batteryPercentage <= 50)
-            return 2; // 26-50%
+            return 2;
         else if (batteryPercentage <= 75)
-            return 3; // 51-75%
+            return 3;
         else
-            return 4; // 76-100%
+            return 4;
     }
 }
