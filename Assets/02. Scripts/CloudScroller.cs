0463121baeaf520443b5a7f29b79feebd3031cdd
aeju <aejuchoe@naver.com>
2024-01-03 22:02:44 +0900

    [main]Refactor: 구름 레이어 가로 너비 직접 입력 -> 자동 계산

--- Assets/02. Scripts/CloudScroller.cs
+++ Assets/02. Scripts/CloudScroller.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 구름 레이어 : 6개
-// 오른쪽으로 스크롤
-// 가장 오른쪽 레이어(레이어6, 마지막 레이어) -> 레이어 width만큼 이동
+// 1. 오른쪽으로 스크롤 : 가장 오른쪽 레이어(레이어6, 마지막 레이어) -> 레이어 width만큼 이동
 // -> 첫번째 레이어(가장 왼쪽)로 이동 (좌표: 첫번째 레이어 - width)
-// 구름 y축으로 움직임 추가 -> 사인파 사용 
+// 3. 구름 y축으로 움직임 추가 -> 사인파 사용 
+// 개선 : 레이어 너비 인스펙터창에서 입력(Layer Spacing) -> 직접 계산하도록 
 public class CloudScroller : MonoBehaviour
 {
     public float scrollSpeed = 0.5f; // 스크롤 속도
     private float originalLastPositionX; // 마지막 레이어의 원래 x좌표
-    public float layerSpacing = 38.0f; // 레이어 간 간격 (레이어 너비)
+    private float layerSpacing; // 레이어 간 간격 (레이어 너비)
     
     public float verticalAmplitude = 0.01f; // 수직 이동의 진폭
     public float verticalFrequency = 1.0f; // 수직 이동의 주파수
 
     void Start()
     {
+        // 첫 번째 자식 레이어의 SpriteRenderer 컴포넌트 가져오기
+        SpriteRenderer spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null && spriteRenderer.sprite != null)
+        {
+            // 스프라이트의 월드 길이 계산 (sprite - size : 픽셀 단위 -> localscale.x 곱해줘서 월드 좌표로 변환)
+            float spriteWorldLength = spriteRenderer.sprite.bounds.size.x * transform.GetChild(0).localScale.x;
+            layerSpacing = spriteWorldLength; // 계산된 길이를 layerSpacing에 할당
+        }
+        
         // 마지막 레이어의 원래 x좌표 저장 
         Transform lastChild = transform.GetChild(transform.childCount - 1); 
         originalLastPositionX = lastChild.position.x;
