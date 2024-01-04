d957e214c88d62d2d16d2a96356b60263aa7bc23
aeju <aejuchoe@naver.com>
2024-01-04 14:46:27 +0900

    [settings]추가: IdleModeBtn
    commit 3fd3af70a768ec332560b5096b71b01e9c0976d6
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Jan 4 14:45:00 2024 +0900
    
        [settings]추가: idleModeBtn (ScreenManager 수정)
    
    commit 3ab879076e7cd9789d66688d773d1a94e21bdf41
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Jan 4 14:26:27 2024 +0900
    
        [settings]수정: screenmanager 변수 (blackscreencanvas -> idlemodecanvas)
    
    commit 7c2634c6b8c7e1c08c1f6d8328048bfc0a41e467
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Dec 28 18:30:56 2023 +0900
    
        [settings]추가: 환경설정 - 상단바 UI 배치
    
    commit a6ba58b35e3ea861604e95aeb572d797572e6e62
    Author: aeju <aejuchoe@naver.com>
    Date:   Thu Dec 28 18:26:24 2023 +0900
    
        [settings]Resource: status bar icon 추가
    
    commit 6d52de5f9481eb294cf51370d2df59db7c7d475d
    Author: aeju <aejuchoe@naver.com>
    Date:   Wed Dec 27 21:01:11 2023 +0900
    
        [Settings]추가: 대략적인 설정UI 배치
    
        - 작업 필요 : 프레임 제한 / 절전 모드 온,오프 / 데미지 폰트 온,오프
    
    commit 6d05499ebf9517f360740eb90137c60aefa548b5
    Author: aeju <aejuchoe@naver.com>
    Date:   Wed Dec 27 21:00:02 2023 +0900
    
        [Settings]추가: 메뉴 버튼 클릭 -> 메뉴 패널 활성화 / 비활성화(닫기)
    
        비활성화 조건 : X btn , 메뉴 패널 밖 터치
    
    commit 360e91fda62a5b13cc8f0652acfdf289fbfbebbc
    Author: aeju <aejuchoe@naver.com>
    Date:   Wed Dec 27 20:58:16 2023 +0900
    
        [Settings]추가: 데미지 폰트 아이콘 이미지
    
    commit e99a766ea193530e79ced696940cb40cff9cb2df
    Author: aeju <aejuchoe@naver.com>
    Date:   Wed Dec 27 20:23:59 2023 +0900
    
        [Settings]추가: 환경설정 / 절전모드 아이콘 이미지

--- Assets/02. Scripts/Manager/ScreenManager.cs
+++ Assets/02. Scripts/Manager/ScreenManager.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// On: 정해진 시간 동안 입력 x, Idle Mode Btn
+// Off: 조이스틱 입력 최댓값
 public class ScreenManager : MonoBehaviour
 {
     public float idleTime = 60.0f; // 방치 시간 : 1분 
     private float currentTime;
     
-    public Canvas blackScreenCanvas; // idle Mode(검은 화면) 표시 Canvas
+    public Canvas idleModeCanvas; // idle Mode(검은 화면) 표시 Canvas
     private CountTime countTime;
     
     private void Start()
     {
-        blackScreenCanvas.enabled = false; 
+        idleModeCanvas.enabled = false; 
         currentTime = 0; // 타이머 초기화
 
         countTime = gameObject.AddComponent<CountTime>();
@@ -26,9 +28,9 @@ public class ScreenManager : MonoBehaviour
         if (Input.anyKey || Input.GetMouseButton(0) || Input.GetMouseButton(1))
         {
             currentTime = 0;
-            if (blackScreenCanvas.enabled)
+            if (idleModeCanvas.enabled)
             {
-                blackScreenCanvas.enabled = false; 
+                idleModeCanvas.enabled = false; 
                 countTime.IdleModeOff();
             }
             
@@ -38,12 +40,23 @@ public class ScreenManager : MonoBehaviour
             currentTime += Time.deltaTime; // 입력이 없을 경우 타이머 증가
 
             // 지정된 방치 시간이 초과되면 검은 화면을 활성화
-            if (currentTime >= idleTime && !blackScreenCanvas.enabled)
+            if (currentTime >= idleTime && !idleModeCanvas.enabled)
             {
-                Debug.Log("Activating Black Screen");
-                blackScreenCanvas.enabled = true; // idle Mode 잠금화면 활성화
+                Debug.Log("Idle Mode Screen On");
+                idleModeCanvas.enabled = true; // idle Mode 잠금화면 활성화
                 countTime.IdleModeOn();
             }
+        }
+    }
+    
+    // IdleModeBtn에서 호출
+    public void ActivateIdleModeScreen()
+    {
+        if (!idleModeCanvas.enabled)
+        {
+            Debug.Log("Idle Mode Screen On (Button)");
+            idleModeCanvas.enabled = true;
+            countTime.IdleModeOn();
         }
     }
 }
