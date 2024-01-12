b5c07fe6f74d38c850db1f469eb37c9afaf1c442
aeju <aejuchoe@naver.com>
2024-01-13 00:21:50 +0900

    [attack]수정: 몬스터 방향 뒤집기
    - 수정 필요 : 재추적

--- Assets/02. Scripts/Monster/Monster.cs
+++ Assets/02. Scripts/Monster/Monster.cs
@@ -5,49 +5,119 @@ using UnityEngine;
 
 // 수정1 : Vector2 -> Vector3로 변경
 // 수정2 : 플레이어 앞에서 따라가기 멈추기
-// 수정3 : 타겟 -> 플레이어 태그로 찾아서 넣기
+// 최소 거리도 필요? 
 public class Monster : MonoBehaviour
 {
     public float speed;
-    public Rigidbody2D target;
+    public GameObject player;
 
     private bool isLive = true;
 
-    private Rigidbody2D rigid;
-    private SpriteRenderer spriter;
-
+    private Rigidbody rigid;
+    
+    public float followDistance = 8.0f; // 플레이어를 따라갈 최대 거리 
+    public float stopDistance = 1.0f; // 목표 지점에 도달했을 때 멈출 거리 
+    
     void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        //spriter = GetComponent<SpriteRenderer>();
-    }
-
-    void FixedUpdate()
-    {
-        if (!isLive)
-            return;
-        
-        // 방향(위치 차이) = 타겟 위치 - 나의 위치
-        Vector2 dirVec = target.position - rigid.position;
-        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
-
-        // 플레이어의 키입력 값을 더한 이동 = 몬스터의 방향 값을 더한 이동
-        rigid.MovePosition(rigid.position + nextVec);
-        //rigid.velocity = Vector2.zero;
+        rigid = GetComponent<Rigidbody>();
     }
     
-    // 몬스터 스프라이트 방향 -> spriteRender x => 다른 방법 찾아봐야함! 
-    void LateUpdate()
-    {
-        if (!isLive)
-            return;
-        // 목표의 x축 값과 자신의 x축 값을 비교하여 작으면 true가 되도록 설정
-        //spriter.flipX = target.position.x < rigid.position.x;
-    }
-
-
     void OnEnable() // 스크립트가 활성화 될 때, 호출되는 이벤트 함수 
     {
-        //target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("player"); // awake에서 작성해야? 
+    }
+
+    // 플레이어의 현재 위치를 계속 추적 + 해당 위치로 몬스터가 이동
+    void FixedUpdate()
+    {
+        if (!isLive || player == null)
+            return;
+        
+        // 방향 계산 (플레이어 위치 - 나의 위치)
+        Vector3 dirVec = player.transform.position - transform.position;
+        
+        // 플레이어와의 거리가 followDistance 이내이면 몬스터가 이동
+        if (dirVec.magnitude < followDistance)
+        {
+            // 목표 지점에 너무 가까우면 이동 멈춤
+            if (dirVec.magnitude > stopDistance)
+            {
+                Vector3 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+                rigid.MovePosition(rigid.position + nextVec);
+            }
+            else
+            {
+                rigid.velocity = Vector3.zero;
+            }
+        }
+        else
+        {
+            rigid.velocity = Vector3.zero; // 플레이어가 최대 거리 밖에 있으면 이동 멈춤
+        }
+        
+        /*
+        // 목표 지점에 가까워지면 이동 멈춤
+        if (dirVec.magnitude < stopDistance)
+        {
+            rigid.velocity = Vector3.zero;
+            //return;
+        }
+        
+        // 플레이어와의 거리가 followDistance 이내 -> 몬스터가 이동
+        else if (dirVec.magnitude < followDistance)
+        {
+            Vector3 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+            rigid.MovePosition(rigid.position + nextVec);
+        }
+
+        else
+        {
+            rigid.velocity = Vector3.zero; // 최대 거리 밖 : 이동 멈추기 
+        }
+        */
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive || player == null)
+            return;
+        
+        // 목표의 x축 값과 자신의 x축 값을 비교하여 작으면 true가 되도록 설정
+        if (player.transform.position.x < transform.position.x)
+            FlipX();
+    }
+    
+    void FlipX()
+    {
+        // 플레이어가 몬스터의 왼쪽에 있고, 몬스터가 오른쪽을 바라보고 있는 경우
+        if (player.transform.position.x < transform.position.x && transform.localScale.x > 0)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1; // X축 스케일을 반전
+            transform.localScale = localScale;
+        }
+        // 플레이어가 몬스터의 오른쪽에 있고, 몬스터가 왼쪽을 바라보고 있는 경우
+        else if (player.transform.position.x > transform.position.x && transform.localScale.x < 0)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1; // X축 스케일을 반전
+            transform.localScale = localScale;
+        }
+        
+        /*
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1; // X축 스케일을 반전
+        transform.localScale = localScale;
+        */
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, followDistance);
+        
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, stopDistance);
     }
 }
