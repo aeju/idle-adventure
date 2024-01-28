b2c7e651f050b3b60fc9992bd95713732fdc3f07
aeju <aejuchoe@naver.com>
2024-01-29 00:32:35 +0900

    [settings]추가: 뒤로가기 버튼으로도 세팅 패널 닫히게
    - 기존 : X button

--- Assets/02. Scripts/UI/Btn/SettingBtn.cs
+++ Assets/02. Scripts/UI/Btn/SettingBtn.cs
@@ -11,45 +11,36 @@ using UnityEngine.EventSystems;
 // 닫힘: X버튼, 패널 밖 영역 터치
 public class SettingBtn : MonoBehaviour
 {
+    public GameObject markerPrefab; // 클릭 마커 프리팹
+    public Canvas canvas; // UI 캔버스
+
     public Button settingBtn; // 메뉴 버튼
     public Button closeBtn; // 클로즈 버튼 
-    
+
     public GameObject settingPanel; // 세팅 패널
     public MenuBtn menuBtnScript;
 
     void Start()
     {
         settingPanel.SetActive(false);
-        
+
         settingBtn.OnClickAsObservable().Subscribe(_ =>
         {
             menuBtnScript.ToggleMenuPanel(); // 메뉴 패널 비활성화
             settingPanel.SetActive(true);
         }).AddTo(this);
-        
-        closeBtn.OnClickAsObservable().Subscribe(_ =>
-        {
-            settingPanel.SetActive(false);
-        }).AddTo(this);
-        
-        // 터치 감지 
-        this.UpdateAsObservable()
-            .Where(_ => settingPanel.activeSelf) // 세팅 패널이 활성화된 상황에서
-            .Where(_ => Input.GetMouseButtonDown(0)) // 터치가 입력될 때만 구독 내부 코드 실행 
+
+        closeBtn.OnClickAsObservable().Subscribe(_ => { settingPanel.SetActive(false); }).AddTo(this);
+
+        // 뒤로가기 키 입력 감지 (= Update)
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.Escape)) // 뒤로가기 키가 눌렸을 때
             .Subscribe(_ =>
             {
-                // UI 중복 확인: 현재 터치가 UI 요소 위에 있는지 확인 
-                if (!EventSystem.current.IsPointerOverGameObject()) 
-                    // 터치가 세팅 패널 밖에 있는 경우, 세팅 패널을 비활성화
-                
-                    if (!RectTransformUtility.RectangleContainsScreenPoint( // 터치 -> 메뉴 패널의 RectTransform 범위 내에 있는지 확인 
-                            settingPanel.GetComponent<RectTransform>(), 
-                            Input.mousePosition, // 터치 현재 위치
-                            Camera.main)) // 스크린 좌표 -> 월드 좌표로 전환 
-                    {
-                        settingPanel.SetActive(false);
-                    }
-                
+                if (settingPanel.activeSelf)
+                {
+                    settingPanel.SetActive(false);
+                }
             }).AddTo(this);
     }
 
