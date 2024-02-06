ba9acfd35b33c384b405b0247cf66954a9d7c4d1
aeju <aejuchoe@naver.com>
2024-02-06 13:50:42 +0900

    [attack]리팩토링: 기본 공격/ 스킬 따로따로 -> 범용 함수

--- Assets/02. Scripts/Player/PlayerController/PlayerController.cs
+++ Assets/02. Scripts/Player/PlayerController/PlayerController.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public interface IPlayerController
 {
-    void PlayerMove();
+    //void PlayerMove();
 }
 
 // 필요 : 공격 -> Move x (state 분리)
@@ -29,7 +29,7 @@ public partial class PlayerController : MonoBehaviour, IPlayerController
     public PlayerStats playerStats;
     
     // 애니메이션
-    private Animator anim;
+    public Animator anim;
     
     // 상태 (생존)
     public bool isAlive = true;
@@ -60,15 +60,14 @@ public partial class PlayerController : MonoBehaviour, IPlayerController
 
     void Start()
     {
+        /*
         anim = GetComponent<Animator>();
-
         HPSliderUpdate();
-
         attackEffect.SetActive(false);
         skillEffect.SetActive(false);
-        
         monsterLayerMask = LayerMask.GetMask("Enemy");
         StartCoroutine(DetectNearestMonsterCoroutine());
+        */
 
         _playerStateContext = new PlayerStateContext(this);
 
@@ -111,39 +110,9 @@ public partial class PlayerController : MonoBehaviour, IPlayerController
         CurrentMoveDirection = direction;
         _playerStateContext.Transition(_moveState);
     }
+    
 
-    void Update()
-    {
-        if (isAlive)
-        {
-            PlayerMove();
-            
-            // z : 기본 공격
-            if (Input.GetKeyDown(KeyCode.Z))
-            {
-                PlayerAttack();
-            }
-            // x : 크리티컬 공격
-            else if (Input.GetKeyDown(KeyCode.X))
-            {
-                PlayerSkill();
-            }
-            
-            else if (Input.GetKeyDown(KeyCode.C)) // 필요시, 치트키 용으로 사용
-            {
-                PlayerSkill();
-            }
-        }
-        
-        // 죽음 -> 나중: GameManager에서 관리 
-        if (isAlive == true && playerStats.currentHP <= 0) // 죽음 1회만 처리하기 위한 플래그
-        {
-            // 애니메이션 -> 죽음
-            anim.SetTrigger("isDead");
-            isAlive = false;
-        }
-    }
-
+    /*
     public void PlayerMove()
     {
         // terrain raycast
@@ -209,9 +178,11 @@ public partial class PlayerController : MonoBehaviour, IPlayerController
             anim.SetBool("isMove", true);
         }
     }
+    */
 
+    
     // 체크 시간 : 3초
-    IEnumerator DetectNearestMonsterCoroutine()
+    public IEnumerator DetectNearestMonsterCoroutine()
     {
         while (true)
         {
@@ -280,83 +251,36 @@ public partial class PlayerController : MonoBehaviour, IPlayerController
         }
     }
     
-    void CreateAttackEffect() // 1.5초 후, 끄기 (수정: 스킬 재사용 시간)
+    // 이펙트, 코루틴을 파라미터로 받는 공통 함수 (일정 시간 후 비활성화)
+    void ActivateEffect(GameObject effect, float duration)
     {
-        attackEffect.SetActive(true);
-        StartCoroutine(DeactivateAttackEffect());
-    }
-    
-    void CreateSkillEffect() // 1.5초 후, 끄기 (수정: 스킬 재사용 시간)
-    {
-        skillEffect.SetActive(true);
-        StartCoroutine(DeactivateSkillEffect());
-    }
-    
-    IEnumerator DeactivateAttackEffect()
-    {
-        yield return new WaitForSeconds(1.5f);
-        attackEffect.SetActive(false);
-    }
-    
-    IEnumerator DeactivateSkillEffect()
-    {
-        yield return new WaitForSeconds(1.5f);
-        skillEffect.SetActive(false);
-    }
-    
-    // 일반 공격 
-    void PlayerAttack()
-    {
-        anim.SetTrigger("AttackTrigger");
-    }
-    
-    // 치명타 공격 (쿨타임 10초) -> 코루틴으로 변경
-    void PlayerSkill()
-    {
-        anim.SetTrigger("SkillTrigger");
-        
-        if (!isSkillOnCooldown)
+        if (effect != null)
         {
-            StartCoroutine(SkillCoroutine());
-        }
-        else
-        {
-            float remainCooldown = (lastSkillTime + skillCooldown) - Time.time;
-            Debug.Log($"남은 시간: {remainCooldown}");
+            effect.SetActive(true);
+            StartCoroutine(DeactivateEffect(effect, duration));
         }
     }
 
-    IEnumerator SkillCoroutine()
+    // 지정된 시간이 지난 후 게임 오브젝트를 비활성화하는 코루틴
+    IEnumerator DeactivateEffect(GameObject effect, float delay)
     {
-        anim.SetTrigger("CriticalAttackTrigger");
-
-        isSkillOnCooldown = true;
-        lastSkillTime = Time.time;
-
-        // 스킬 쿨타임 슬라이더
+        yield return new WaitForSeconds(delay);
+        if (effect != null)
         {
-            if (cooldownSlider != null) // 슬라이더 초기화 
-            {
-                cooldownSlider.value = 0;
-                cooldownSlider.maxValue = skillCooldown;
-            }
-
-            // 쿨타임 동안 슬라이더 업데이트
-            while (Time.time < lastSkillTime + skillCooldown)
-            {
-                if (cooldownSlider != null)
-                {
-                    cooldownSlider.value = Time.time - lastSkillTime;
-                }
-                yield return null; // 다음 프레임까지 기다리도록 보장
-            }
-            isSkillOnCooldown = false;
-
-            if (cooldownSlider != null)
-            {
-                cooldownSlider.value = cooldownSlider.maxValue;
-            }
+            effect.SetActive(false);
         }
+    }
+    
+    // 기본 공격 이펙트
+    void CreateAttackEffect()
+    {
+        ActivateEffect(attackEffect, 1.5f); 
+    }
+
+    // 스킬 공격 이펙트
+    void CreateSkillEffect()
+    {
+        ActivateEffect(skillEffect, 1.5f); 
     }
 
     // 피격 함수
