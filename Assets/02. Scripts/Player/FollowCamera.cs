85785b1b3093d0a63b49b4a4614d60ed61f82cde
aeju <aejuchoe@naver.com>
2024-02-08 23:53:00 +0900

    [attack]수정: FollowCam - 플레이어 할당 자동으로
    - 조건: PlayerController 보유
    - 문제: 실행 순서 보장 (손 봐야함!)

--- Assets/02. Scripts/Player/FollowCamera.cs
+++ Assets/02. Scripts/Player/FollowCamera.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 // 기존 FollowCam 문제점 : 실행되면, 기존 카메라 위치/각도 유지 x
+// 문제: target찾긴 하는데, 에러뜸! (실행순서 보장 필요) 
 public class FollowCamera : MonoBehaviour
 {
     public Transform target; 
@@ -10,6 +11,17 @@ public class FollowCamera : MonoBehaviour
     
     void Start()
     {
+        PlayerController playerController = FindObjectOfType<PlayerController>();
+        if (playerController != null)
+        {
+            // Assign the player's transform to the target
+            target = playerController.transform;
+        }
+        else
+        {
+            Debug.LogError("PlayerController not found in the scene.");
+        }
+        
         // initial offset : 카메라 - 플레이어 위치
         offset = transform.position - target.position;
         
