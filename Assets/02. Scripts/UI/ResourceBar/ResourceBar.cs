b5ec1949602f2e850df60be5b75c244ce515156e
aeju <aejuchoe@naver.com>
2024-02-28 18:31:37 +0900

    [enforce]리팩토링: 코인 표현 방식 클래스 분리
    - resourcebar -> coinformatter

--- Assets/02. Scripts/UI/ResourceBar/ResourceBar.cs
+++ Assets/02. Scripts/UI/ResourceBar/ResourceBar.cs
@@ -69,7 +69,8 @@ public class ResourceBar : MonoBehaviour
             coin = resoureInfo.current_Coin;
 
             rubyText.text = ruby.ToString();
-            coinText.text = FormatCoinUnit(coin);
+            //coinText.text = FormatCoinUnit(coin);
+            coinText.text = CoinFormatter.FormatCoinUnit(coin);
         }
     }
 
@@ -85,53 +86,6 @@ public class ResourceBar : MonoBehaviour
             int combatPower = player.playerStats.combatPower;
             
             combatPowerText.text = combatPower.ToString();
-        }
-    }
-
-    // K(Kilo 킬로) : 1,000
-    // M(Mega 메가) : 1,000,000
-    // G(Giga 기가) : 1,000,000,000
-    // T(Tera 테라) : 1,000,000,000,000
-    // P(Peta 페타) : 1,000,000,000,000,000
-    // E(Exa 엑사) : 1,000,000,000,000,000,000
-    // Z(Zetta 제타) : 1,000,000,000,000,000,000,000
-    // Y(Yotta 요타) : 1,000,000,000,000,000,000,000,000
-    // 각 단위 : 1000의 거듭제곱 
-    string FormatCoinUnit(int number)
-    {
-        if (number >= 1000) 
-        {
-            return (number / 1000f).ToString("0.#") + "K"; // 킬로
-        }
-        
-        else if (number >= 1000000)
-        {
-            return (number / 1000000f).ToString("0.#") + "M"; // 메가
-        }
-
-        else if (number >= 1000000000)
-        {
-            return (number / 1000000000f).ToString("0.#") + "G"; // 기가
-        }
-
-        else if (number >= 1000000000000) 
-        {
-            return (number / 1000000000000f).ToString("0.#") + "T"; // 테라
-        }
-        
-        else if (number >= 1000000000000000) 
-        {
-            return (number / 1000000000000000f).ToString("0.#") + "P"; // 페타
-        }
-        
-        else if (number >= 1000000000000000000) 
-        {
-            return (number / 1000000000000000000f).ToString("0.#") + "E"; // 엑사
-        }        
-        
-        else
-        {
-            return number.ToString(); // 1000이하 : 축약할 필요 x
         }
     }
 }
