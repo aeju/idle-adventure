bc7a4e0604748d2328e394a18c21e178c3b1dce8
aeju <aejuchoe@naver.com>
2024-03-02 19:28:05 +0900

    [enforce]리팩토링 작업 중: UpgradeOption 클래스 분리
    - 할 것 : UpgradeOption 인스턴스 인스펙트에서 구성

--- Assets/02. Scripts/Player/PlayerEnforce.cs
+++ Assets/02. Scripts/Player/PlayerEnforce.cs
@@ -4,30 +4,165 @@ using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
+using System;
 
+[Serializable]
 public class UpgradeOption
 {
-    
+    public Button upgradeButton;
+    public int increaseAmount;
+    public int cost = 100;
+    public TextMeshProUGUI costText, levelText, totalIncreaseText;
+    public int level = 0;
+    public int totalIncrease = 0;
+
+    // 업그레이드 실행을 위한 메서드
+    public void Upgrade(Action upgradeAction)
+    {
+        if (ResourceManager.Instance.current_Coin >= cost)
+        {
+            upgradeAction(); // 업그레이드
+            ResourceManager.Instance.current_Coin -= cost;
+            
+            totalIncrease += increaseAmount;
+            cost++; 
+            level++;
+
+            // UI 업데이트
+            costText.text = NumberFormatter.FormatNumberUnit(cost);
+            levelText.text = "Lv. " + level.ToString();
+            totalIncreaseText.text = "+" + totalIncrease;
+        }
+        else // 보유 코인 부족
+        {
+            Debug.Log("Not enough coins");
+        }
+    }
 }
 
 public class PlayerEnforce : EnforceSubject
 {
+    //public UpgradeOption attackUpgrade, maxHPUpgrade, defenseUpgrade;
+    private UpgradeOption attackUpgrade, maxHPUpgrade, defenseUpgrade;
+    
+    private PlayerStats playerStats;
+    
+    // 옵저버 패턴
+    private ResourceBar resourceBar;
+    
+    // 경고 팝업
+    [SerializeField] private GameObject alertPopup;
+    
+    void Awake()
+    {
+        // 기존
+        playerStats = FindObjectOfType<PlayerStats>();
+        resourceBar = FindObjectOfType<ResourceBar>();
+
+        if (alertPopup != null)
+        {
+            //alertPopup.SetActive(false);
+            alertPopup.SetActive(true);
+        }
+        
+        // new
+        attackUpgrade = new UpgradeOption
+        {
+            upgradeButton = attackBtn,
+            increaseAmount = attackIncrease,
+            cost = attackCost,
+            costText = attackCostText,
+            levelText = attackLevelText,
+            totalIncreaseText = totalAttackIncreaseText
+        };
+        
+        maxHPUpgrade = new UpgradeOption
+        {
+            upgradeButton = maxHPBtn,
+            increaseAmount = maxHPIncrease,
+            cost = maxHPCost,
+            costText = maxHPCostText,
+            levelText = maxHPLevelText,
+            totalIncreaseText = totalMaxHPIncreaseText
+        };
+        
+        defenseUpgrade = new UpgradeOption
+        {
+            upgradeButton = defenseBtn,
+            increaseAmount = defenseIncrease,
+            cost = defenseCost,
+            costText = defenseCostText,
+            levelText = defenseLevelText,
+            totalIncreaseText = totalDefenseIncreaseText
+        };
+    }
+
+    void Start()
+    {
+        attackUpgrade.upgradeButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            attackUpgrade.Upgrade(() =>
+            {
+                playerStats.attack += attackUpgrade.increaseAmount;
+                totalAttackIncrease += attackUpgrade.increaseAmount;
+            });
+            NotifyObservers();
+        }).AddTo(this);
+        
+        maxHPUpgrade.upgradeButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            maxHPUpgrade.Upgrade(() =>
+            {
+                playerStats.maxHP += maxHPUpgrade.increaseAmount;
+                totalMaxHPIncrease += maxHPUpgrade.increaseAmount;
+            });
+            NotifyObservers();
+        }).AddTo(this);
+        
+        defenseUpgrade.upgradeButton.OnClickAsObservable().Subscribe(_ =>
+        {
+            defenseUpgrade.Upgrade(() =>
+            {
+                playerStats.defense += defenseUpgrade.increaseAmount;
+                totalDefenseIncrease += defenseUpgrade.increaseAmount;
+            });
+            NotifyObservers();
+        }).AddTo(this);
+    }
+    
+    // 관찰자 연결 활성화, 비활성화
+    void OnEnable()
+    {
+        if (resourceBar)
+        {
+            Debug.Log("Attach1");
+            Attach(resourceBar);
+        }
+    }
+
+    void OnDisable()
+    {
+        if (resourceBar)
+            Detach(resourceBar);
+    }
+    
+    
     // 버튼 (공격력/HP/방어력)
+    //[SerializeField] private Button attackBtn, maxHPBtn, defenseBtn;
     [SerializeField] private Button attackBtn, maxHPBtn, defenseBtn;
     
     // 업그레이드 증가량
     public int attackIncrease = 2;
-    public int hpIncrease = 15;
+    public int maxHPIncrease = 15;
     public int defenseIncrease = 1;
     
     // 총 증가량  
-    private int totalAttackIncrease = 0;
-    private int totalHpIncrease = 0;
-    private int totalDefenseIncrease = 0 ;
+    public int totalAttackIncrease = 0;
+    public int totalMaxHPIncrease = 0;
+    public int totalDefenseIncrease = 0 ;
 
     public TextMeshProUGUI totalAttackIncreaseText;
-    public TextMeshProUGUI totalHpIncreaseText;
+    public TextMeshProUGUI totalMaxHPIncreaseText;
     public TextMeshProUGUI totalDefenseIncreaseText;
 
     // 골드 비용
@@ -40,21 +175,21 @@ public class PlayerEnforce : EnforceSubject
     public TextMeshProUGUI defenseCostText;
     
     // 레벨
-    public int attackLevel = 0;
-    public int maxHPLevel = 0;
-    public int defenseLevel = 0;
+    //public int attackLevel = 0;
+    //public int maxHPLevel = 0;
+    //public int defenseLevel = 0;
 
     public TextMeshProUGUI attackLevelText;
     public TextMeshProUGUI maxHPLevelText;
     public TextMeshProUGUI defenseLevelText;
     
+    /*
     private ResourceManager resourceInfo;
     
     // 옵저버 패턴
     private PlayerStats playerStats;
     
     private ResourceBar resourceBar;
-    private HeroStatsUI heroStatsUI; 
     
     // 경고 팝업
     [SerializeField] private GameObject alertPopup;
@@ -200,19 +335,6 @@ public class PlayerEnforce : EnforceSubject
         }
     }
 
-    // 관찰자 연결 활성화, 비활성화
-    void OnEnable()
-    {
-        if (resourceBar)
-        {
-            Debug.Log("Attach1");
-            Attach(resourceBar);
-        }
-    }
-
-    void OnDisable()
-    {
-        if (resourceBar)
-            Detach(resourceBar);
-    }
+    
+    */
 }
