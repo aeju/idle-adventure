7cfd079cf0a685efe877333912123ef55deefc57
aeju <aejuchoe@naver.com>
2024-03-22 23:54:17 +0900

    [buff]추가: 광고형 보상 시청 완료 변수

--- Assets/02. Scripts/Manager/RewardAdManager.cs
+++ Assets/02. Scripts/Manager/RewardAdManager.cs
@@ -8,6 +8,8 @@ using UniRx;
 
 public class RewardAdManager : MonoBehaviour
 {
+    private bool isAdWatched; // 광고 시청 여부 
+    
     [SerializeField] private GameObject rewardPopup; 
     
     [SerializeField] Button rewardADBtn;
@@ -21,9 +23,10 @@ public class RewardAdManager : MonoBehaviour
     // 버프 활성화 이벤트
     public static event Action OnBuffActivated;
     
-    
-    void Stat()
+    void Start()
     {
+        isAdWatched = false;
+        
         // 초기화
         MobileAds.Initialize(initStatus => { });
         adRequest = new AdRequest.Builder().Build();
@@ -31,29 +34,10 @@ public class RewardAdManager : MonoBehaviour
         // 보상형 광고를 미리 로드
         LoadRewardedAd();
         
-        
         // rewardADBtn 버튼 - 클릭 리스너 추가
         rewardADBtn.onClick.AddListener(ShowRewardedAd);
-        
-        /*
-        // 팝업부터 띄우기
-        if (rewardPopup != null)
-        {
-            rewardPopup.SetActive(false);
-        }
-        */
     }
 
-    void OnEnable()
-    {
-        Debug.Log("1. rewardPopup.SetActive(false);");
-        // 팝업 끄기 
-        if (rewardPopup != null)
-        {
-            Debug.Log("2. rewardPopup.SetActive(false);");
-            rewardPopup.SetActive(false);
-        }
-    }
     
     void LoadRewardedAd(bool show = false)
     {
@@ -84,48 +68,46 @@ public class RewardAdManager : MonoBehaviour
         });
     }
 
-    public void ShowRewardedAd()
+    void ShowRewardedAd()
     {
-        Debug.Log("Btn Click1");
         // 보상형 광고가 로드되었고, 광고를 표시할 수 있는 상태인지 확인
         if (rewardedAd != null && rewardedAd.CanShowAd())
         {
             // 보상형 광고 표시
             rewardedAd.Show(RewardHandler);
-            Debug.Log("Btn Click2");
         }
         else
         {
             // 보상형 광고를 로드하지 않았거나 광고를 표시할 수 없는 상태라면 다시 로드
             LoadRewardedAd(true);
-            Debug.Log("Btn Click4");
         }
     }
 
     // 콜백 함수 (광고를 본 후 보상 처리)
     void RewardHandler(Reward reward)
     {
-        Debug.Log($"RewardHandler called. Popup active state before set: {rewardPopup.activeSelf}");
-        Debug.Log("Btn Click5");
-        //Debug.Log($"RewardHandler called. Popup active state before set: {rewardPopup.activeSelf}");
-        rewardPopup.SetActive(true);
-        /*
-        // 팝업부터 띄우기
         if (rewardPopup != null)
         {
-            rewardPopup.SetActive(true);
-        }*/
-        
-        /*
-        // CoinBuff 활성화
-        if (coinBuff != null)
-        {
-            // 보상 -> 코루틴 X, 중간 한 단계 더 필요 
-            coinBuff.Activate();
-            
-            // 버프 활성화 이벤트 발생
-            OnBuffActivated?.Invoke();
+            isAdWatched = true;
+            Debug.Log("Click2");
         }
-        */
+        else
+        {
+            Debug.Log("rewardPopupActive is null");
+        }
+    }
+
+    void Update()
+    {
+        if (isAdWatched && !rewardPopup.activeSelf)
+        {
+            rewardPopup.SetActive(true);
+            Debug.Log("isAdWatched");
+        }
+
+        else
+        {
+            rewardPopup.SetActive(false);
+        }
     }
 }
