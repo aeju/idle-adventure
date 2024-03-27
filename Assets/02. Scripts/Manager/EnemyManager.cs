22b19de6234d8ea525eb6b8afd39e97acdf612c5
aeju <aejuchoe@naver.com>
2024-03-27 20:50:50 +0900

    [attack]수정: 변수, 메서드 분리

--- Assets/02. Scripts/Manager/EnemyManager.cs
+++ Assets/02. Scripts/Manager/EnemyManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 // 랜덤 위치 생성
 // 같은 spawnpoint에 생기지 x
+
+// 제일 처음 : 생성된 상태
+// 그 후 : 리스폰 타임 이후 생성 
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager Instance { get; private set; }
@@ -19,8 +22,8 @@ public class EnemyManager : MonoBehaviour
     // 적 공장
     public GameObject enemyFactory;
     
-    // 오브젝트 풀 크기
-    public int poolSize = 10;
+    // 최대 몬스터 수 (오브젝트 풀 크기)
+    public int maxMonsters = 10;
     // 오브젝트 풀 배열
     public List<GameObject> enemyObjectPool;
     // SpawnPoint들
@@ -41,19 +44,26 @@ public class EnemyManager : MonoBehaviour
     
     void Start()
     {
-        createTime = Random.Range(minTime, maxTime);
-        // 오브젝트 풀 생성
+        CreateMonsterPool();
+    }
+
+    // 오브젝트 풀 생성
+    void CreateMonsterPool()
+    {
         enemyObjectPool = new List<GameObject>();
         
         // 오브젝트 풀에 넣을 에너미 개수만큼 반복해서
-        for (int i = 0; i < poolSize; i++)
+        for (int i = 0; i < maxMonsters; i++)
         {
             // 에너미 공장에서 에너미 생성
             GameObject enemy = Instantiate(enemyFactory);
-            // 에너미를 오브젝트 풀에 넣기
-            enemyObjectPool.Add(enemy);
+            // 몬스터 이름 지정 (Monster_01 ... )
+            enemy.name = $"Monster_{i + 1:00}";
             // 비활성화
             enemy.SetActive(false);
+            
+            // 에너미를 오브젝트 풀에 넣기
+            enemyObjectPool.Add(enemy);
         }
     }
 
@@ -66,6 +76,12 @@ public class EnemyManager : MonoBehaviour
             // 오브젝트풀에 에너미가 있다면
             if (enemyObjectPool.Count > 0)
             {
+                // 몬스터가 생성되지 않은 spawnPoints 인덱스 저장 리스트
+                List<int> availablespawnPoints = new List<int>();
+                
+                
+                
+                
                 // 오브젝트풀에서 enemy를 가져다 사용
                 GameObject enemy = enemyObjectPool[0];
                 // 오브젝트풀에서 에너미 제거
