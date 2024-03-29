68afad20cf86968d717fc45b3d6fad329a960190
aeju <aejuchoe@naver.com>
2024-03-29 16:22:08 +0900

    [buff]수정: 변수 이름 coin, exp buff로

--- Assets/02. Scripts/UI/MainUI/MainUI.cs
+++ Assets/02. Scripts/UI/MainUI/MainUI.cs
@@ -13,39 +13,35 @@ public class MainUI : MonoBehaviour
     [SerializeField] private Button Hero_Btn;
     [SerializeField] private Button Inventory_Btn;
     [SerializeField] private Button Strengthen_Btn;
-    [SerializeField] private Button Contents_Btn;
-    [SerializeField] private Button Mission_Btn;
+    [SerializeField] private Button CoinBuff_Btn;
+    [SerializeField] private Button ExpBuff_Btn;
     [SerializeField] private Button Shop_Btn;
 
     [SerializeField] private GameObject Hero_Page;
     [SerializeField] private GameObject Inventory_Page;
     [SerializeField] private GameObject Strengthen_Page;
-    [SerializeField] private GameObject Contents_Page;
-    [SerializeField] private GameObject Mission_Page;
+    [SerializeField] private GameObject CoinBuff_Page;
+    [SerializeField] private GameObject ExpBuff_Page;
     [SerializeField] private GameObject Shop_Page;
 
     private GameObject currentOpenPage = null; // 현재 열려있는 페이지
-
-    // X버튼 누를 때! 
-    private List<GameObject> pages;
     
-    // 버튼-페이지 딕셔너리
-    private Dictionary<Button, GameObject> buttonPage;
+    private List<GameObject> pages; // X버튼 누를 때! 
     
-    // 1. X버튼 비활성화
-    // 2. 모든 페이지 비활성화
-    // 3. 버튼 - 페이지 연결
+    private Dictionary<Button, GameObject> buttonPage; // 버튼-페이지 딕셔너리
+    
+    // 페이지 닫기 : X버튼 / ABB키
     private void Start()
     {
-        XbtnClose(); // 1. 초기 X 버튼: 비활성화
+        XbtnClose(); // 1. X 버튼: 비활성화
         
         pages = new List<GameObject>
         {
             Hero_Page,
             Inventory_Page,
             Strengthen_Page,
-            Contents_Page,
-            Mission_Page,
+            CoinBuff_Page,
+            ExpBuff_Page,
             Shop_Page
         };
 
@@ -54,14 +50,14 @@ public class MainUI : MonoBehaviour
             page.SetActive(false);
         }
         
-        // 3. 버튼 - 페이지 매핑, 구독 목적
+        // 3. 버튼 - 페이지 매핑 (구독 목적)
         buttonPage = new Dictionary<Button, GameObject>
         {
             { Hero_Btn, Hero_Page },
             { Inventory_Btn, Inventory_Page },
             { Strengthen_Btn, Strengthen_Page },
-            { Contents_Btn, Contents_Page },
-            { Mission_Btn, Mission_Page },
+            { CoinBuff_Btn, CoinBuff_Page },
+            { ExpBuff_Btn, ExpBuff_Page },
             { Shop_Btn, Shop_Page }
         };
         
@@ -73,6 +69,7 @@ public class MainUI : MonoBehaviour
             }).AddTo(this);
         } 
         
+        // 1) X버튼 눌러서 닫기
         X_btn.OnClickAsObservable().Subscribe(_ =>
         {
             CloseAllPages(); // 무슨 창이든 닫기게
@@ -91,43 +88,28 @@ public class MainUI : MonoBehaviour
         }
     }
     
-    // 버프 활성화 시 실행되는 메서드
+    // 버프 활성화 시 실행
     private void ClosePagesOnBuffActivate(Buff buff)
     {
-        // 버프 종류에 따라 특정 페이지 닫기
+        // 버프 종류에 따라 열린 페이지 닫기
         switch (buff.buffType)
         {
             case BuffType.Coin:
-                // 코인 버프 활성화 시 Contents_Page 닫기
-                if (currentOpenPage == Contents_Page)
+                // 코인 버프 활성화 시 CoinBuff_Page 닫기
+                if (currentOpenPage == CoinBuff_Page)
                 {
                     CloseAllPages();
                 }
                 break;
             case BuffType.Exp:
-                // 경험치 버프 활성화 시 Mission_Page 닫기
-                if (currentOpenPage == Mission_Page)
+                // 경험치 버프 활성화 시 ExpBuff_Page 닫기
+                if (currentOpenPage == ExpBuff_Page)
                 {
                     CloseAllPages();
                 }
                 break;
         }
     }
-    
-    /*
-    // Buff 인자를 받지만, 실제로 사용하지 않음
-    void OnEnable()
-    {
-        //BuffManager.Instance.OnBuffActivated += CloseAllPages;
-        BuffManager.Instance.OnBuffActivated += (buff) => CloseAllPages();
-    }
-
-    void OnDisable()
-    {
-        //BuffManager.Instance.OnBuffActivated -= CloseAllPages;
-        BuffManager.Instance.OnBuffActivated -= (buff) => CloseAllPages();
-    }
-    */
     
     private void OpenPage(GameObject page)
     {
@@ -141,17 +123,18 @@ public class MainUI : MonoBehaviour
     {
         if (currentOpenPage != null) // 페이지가 열려있는 경우에만 
         {
-            currentOpenPage.SetActive(false);
+            currentOpenPage.SetActive(false); // 현재 열려있는 페이지 닫기
             currentOpenPage = null;
             XbtnClose();
         }
     }
     
-    // 뒤로가기 = 페이지 닫기
+    // 2) ABB키 -> 페이지 닫기
     private void Update()
     {
         // 어떤 페이지가 열려있고, 뒤로가기를 누를 때 
-        if (currentOpenPage != null && Input.GetKeyDown(KeyCode.Escape))
+        if (currentOpenPage != null 
+            && Input.GetKeyDown(KeyCode.Escape))
         {
             CloseAllPages();
         }
