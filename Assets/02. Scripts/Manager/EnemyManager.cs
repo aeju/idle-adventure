86793261f228089e56331130784e848ba1b24482
aeju <aejuchoe@naver.com>
2024-04-03 01:33:53 +0900

    [attack]수정: 첫 소환 - 전부 / 그 후 : 지정된 시간 지나면

--- Assets/02. Scripts/Manager/EnemyManager.cs
+++ Assets/02. Scripts/Manager/EnemyManager.cs
@@ -19,24 +19,22 @@ public class EnemyManager : Singleton<EnemyManager>
     private float currentTime; // 경과 시간 추적
     public GameObject monsterPrefab;
 
+    private bool isFirstSpawn = true; // 첫 소환을 위한 플래그
     // 다음 적 생성까지의 시간
     public float createTime = 1;
-
     // 적 생성 최소 대기 시간
     public float minTime = 0.5f;
-
     // 적 생성 최대 대기 시간
     public float maxTime = 1.5f;
     
     // 최대 몬스터 수 (오브젝트 풀 크기)
-    public int maxMonsters = 3;
+    private int maxMonsters = 3;
+    public int basicMonsters = 3;
     public int monsterOffset = 2;
     
-    // 오브젝트 풀 배열 (생성된 적 보관)
-    public List<GameObject> enemyObjectPool;
-
-    // 적이 생성될 위치 (SpawnPoint들)
-    public Transform[] spawnPoints;
+    public List<GameObject> enemyObjectPool = new List<GameObject>(); // 오브젝트 풀 배열 (생성된 적 보관)
+    
+    public Transform[] spawnPoints; // 적이 생성될 위치 (SpawnPoint들)
 
     // 사용된 spawnPoints 인덱스 추적을 위한 리스트
     private List<int> usedSpawnPoints  = new List<int>();
@@ -45,46 +43,40 @@ public class EnemyManager : Singleton<EnemyManager>
     void Start()
     {
         CreateMonsterPool();
+        SpawnMonster(); // 게임 시작 시 바로 몬스터 생성
     }
-
-    
     
     // 오브젝트 풀 생성
     void CreateMonsterPool()
-    {
-        CalculateMaxMonsters(); // 몬스터 수 지정값 안에서 랜덤
+    { 
+        // 몬스터 수 지정값 안에서 랜덤
+        int minMonster = basicMonsters - monsterOffset;
+        int maxMonster = basicMonsters + monsterOffset;
+        maxMonsters = Random.Range(minMonster, maxMonster + 1);
         
-        enemyObjectPool = new List<GameObject>();
-        
-        // 오브젝트 풀에 넣을 에너미 개수만큼 반복해서
-        for (int i = 0; i < maxMonsters; i++)
+        for (int i = 0; i < maxMonsters; i++) // 오브젝트 풀에 넣을 에너미 개수만큼 반복해서
         {
             GameObject enemy = Instantiate(monsterPrefab); // 몬스터 프리팹을 인스턴스화하여 생성 
-            enemy.name = $"Monster_{i + 1:00}"; // 몬스터 이름 지정 (Monster_01 ... )
+            enemy.name = $"{monsterPrefab.name}_{i + 1:00}"; // 몬스터 이름 지정 (프리팹 이름_01 ... )
             enemy.SetActive(false); // 초기 상태 : 비활성화
             
             enemyObjectPool.Add(enemy); // 오브젝트 풀에 몬스터 추가
         }
     }
     
-    void CalculateMaxMonsters()
-    {
-        int minMonster = maxMonsters - monsterOffset;
-        int maxMonster = maxMonsters + monsterOffset;
-        maxMonsters = Random.Range(minMonster, maxMonster + 1);
-    }
-
     void Update()
     {
         // 시간 업데이트
         currentTime += Time.deltaTime;
 
-        // 지정된 시간이 지나면 적 생성
-        if (currentTime > createTime)
+        // 처음 : 곧바로 / 그 후 : 지정된 시간이 지나면 적 생성
+        //if (currentTime > createTime)
+        if (isFirstSpawn || currentTime > createTime)
         {
             SpawnMonster();
             createTime = Random.Range(minTime, maxTime); // 다음 생성 시간 랜덤 설정
             currentTime = 0; // 시간 초기화
+            isFirstSpawn = false; // 첫 소환 후 플래그를 false로 설정
         }
     }
 
