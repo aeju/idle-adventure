973551638ae5a6948e2a2b9ed0356f318ab02503
aeju <aejuchoe@naver.com>
2024-04-12 20:04:34 +0900

    [UI]추가: 자동 포션 UI
    commit 8854bbefced74c85e03a2055681b82e3fc1ba7c2
    Author: aeju <aejuchoe@naver.com>
    Date:   Fri Apr 12 19:35:02 2024 +0900
    
        Reapply "[UI]배치: 자동 포션 UI (01_UIScene)"
    
        This reverts commit 1d56307b04ca61e46a4fb846115c7bfdb915449d.
    
    commit 1d56307b04ca61e46a4fb846115c7bfdb915449d
    Author: aeju <aejuchoe@naver.com>
    Date:   Fri Apr 12 19:34:30 2024 +0900
    
        Revert "[UI]배치: 자동 포션 UI (01_UIScene)"
    
        This reverts commit 169799ea5664434857a07be7786c3e1b054d37d5.
    
    commit a0e1a79fe784579cda24d651dc5b4f7f4767f851
    Author: aeju <aejuchoe@naver.com>
    Date:   Fri Apr 12 19:10:22 2024 +0900
    
        [UI]배치: 자동 포션 UI (01_UIScene)
    
    commit 3652f4a2285c583719ca59929b4dafd251a0d2f4
    Author: aeju <aejuchoe@naver.com>
    Date:   Fri Apr 12 00:19:45 2024 +0900
    
        [UI]수정: 게임 시작 1초 후 텍스트가 할당되는 문제

--- Assets/02. Scripts/UI/IdleMode/TimeTextHourMinSec.cs
+++ Assets/02. Scripts/UI/IdleMode/TimeTextHourMinSec.cs
@@ -10,24 +10,4 @@ public class TimeTextHourMinSec : TimeTextUpdater
     {
         return Utilities.GetCurrentTimeKST().ToString("HH:mm:ss");
     }
-    
-    /*
-    [SerializeField] private TextMeshProUGUI timeTextHourMinSec;
-    private float timeSinceLastUpdate = 0f; // 마지막 업데이트 이후 경과 시간
-    
-    void Update()
-    {
-        timeSinceLastUpdate += Time.deltaTime;
-        
-        // 1초마다 시간을 업데이트
-        if (timeSinceLastUpdate >= 1f)
-        {
-            if (timeTextHourMinSec != null)
-            {
-                timeTextHourMinSec.text = Utilities.GetCurrentTimeKST().ToString("HH:mm:ss");
-            }
-            timeSinceLastUpdate = 0f;  // 마지막 업데이트 시간을 재설정
-        }
-    }
-    */
 }
--- Assets/02. Scripts/UI/StatusBar/TimeTextHourMin.cs
+++ Assets/02. Scripts/UI/StatusBar/TimeTextHourMin.cs
@@ -6,28 +6,17 @@ using TMPro;
 // HH:mm (상단바) 
 public class TimeTextHourMin : TimeTextUpdater
 {
+    // 게임 시작 시 바로 현재 시간으로 초기화
+    void Start()
+    {
+        if (timeText != null)
+        {
+            timeText.text = GetTimeString();
+        }
+    }
+    
     protected override string GetTimeString()
     {
         return Utilities.GetCurrentTimeKST().ToString("HH:mm");
     }
-    
-    /*
-    [SerializeField] private TextMeshProUGUI timeTextHourMin;
-    private float timeSinceLastUpdate = 0f; // 마지막 업데이트 이후 경과 시간
-    
-    void Update()
-    {
-        timeSinceLastUpdate += Time.deltaTime;
-        
-        // 1초마다 시간을 업데이트
-        if (timeSinceLastUpdate >= 1f)
-        {
-            if (timeTextHourMin != null)
-            {
-                timeTextHourMin.text = Utilities.GetCurrentTimeKST().ToString("HH:mm");
-            }
-            timeSinceLastUpdate = 0f;  // 마지막 업데이트 시간을 재설정
-        }
-    }
-    */
 }
