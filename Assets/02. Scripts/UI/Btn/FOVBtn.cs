e26cd5e19f7cf1ae43520711293d0ca5cf0a9659
aeju <aejuchoe@naver.com>
2024-04-21 14:18:06 +0900

    [UI]리팩토링: nextFOV를 사용하여 FOV 변경 로직 명확화
    기존 : 버튼 클릭 -> currentFOV를 변화(현재 시야각, 다음 시야각 역할 전부함)

--- Assets/02. Scripts/UI/Btn/FOVBtn.cs
+++ Assets/02. Scripts/UI/Btn/FOVBtn.cs
@@ -11,11 +11,16 @@ public class FOVBtn : MonoBehaviour
 {
     [SerializeField] private Camera playerCam;
     [SerializeField] private Button fovBtn;
-
+    
     private int currentFovState = 0; 
     private readonly int[] fovStates = { 35, 45, 25 };
     
-    // [UI]
+    [SerializeField] private float fovChangeSpeed = 5f; // FOV 변화 속도 조절
+    [SerializeField] private float currentFOV; // 현재 시야각
+    [SerializeField] private float nextFOV; // 다음 시야각
+    
+    
+    [Header("# UI")]
     [SerializeField] private GameObject firstOn;
     [SerializeField] private GameObject firstOff;
 
@@ -25,19 +30,19 @@ public class FOVBtn : MonoBehaviour
     [SerializeField] private GameObject thirdOn;
     [SerializeField] private GameObject thirdOff;
     
-    [SerializeField] private float currentFOV; // 현재 시야각
-    [SerializeField] private float fovChangeSpeed = 5f; // FOV 변화 속도 조절
-    
     void Start()
     {
         fovBtn.OnClickAsObservable().Subscribe(_ =>
         {
-            ChangeFOV(); // currentFOV 변경
+            ChangeFOV(); // currentFOV = nextFOV로 변경
         }).AddTo(this);
         
         // 초기 시야각 설정
         currentFOV = fovStates[currentFovState]; 
         playerCam.fieldOfView = currentFOV; 
+        
+        // 다음 시야각 (fovStates[currentFovState + 1])
+        nextFOV = GetNextFOV(); 
         
         UpdateUI(currentFovState); // UI 초기화
     }
@@ -53,10 +58,16 @@ public class FOVBtn : MonoBehaviour
 
     private void ChangeFOV()
     {
+        currentFOV = nextFOV; // 현재 시야각을 다음 시야각으로 업데이트
         currentFovState = (currentFovState + 1) % fovStates.Length; // 다음 FOV로 전환
-        currentFOV = fovStates[currentFovState];
+        nextFOV = GetNextFOV(); // 다음 시야각 미리 계산 
         
         UpdateUI(currentFovState); // UI 업데이트
+    }
+    
+    private float GetNextFOV()
+    {
+        return fovStates[(currentFovState + 1) % fovStates.Length]; // 다음 FOV 계산
     }
 
     private void UpdateUI(int fovState)
@@ -84,6 +95,7 @@ public class FOVBtn : MonoBehaviour
                 secondOff.SetActive(true);
                 thirdOff.SetActive(true);
                 break;
+            
         }
     }
 }
