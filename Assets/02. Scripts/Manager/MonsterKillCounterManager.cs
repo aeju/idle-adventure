c2a830cce633a6e63ce89a1c4792748a186ee1aa
aeju <aejuchoe@naver.com>
2024-04-29 12:47:26 +0900

    [attack]추가: 몬스터 처치 수 카운트
    - 몬스터 죽음 -> 이벤트 발생

--- Assets/02. Scripts/Manager/MonsterKillCounterManager.cs
+++ Assets/02. Scripts/Manager/MonsterKillCounterManager.cs
@@ -1,17 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+using UniRx;
 
-public class MonsterKillCounterManager : MonoBehaviour
+// 1. 시간(초) 재기
+// 2. 리셋 버튼 : 시간 리셋
+// (이후, 절전모드일 때 몬스터 카운트)
+public class MonsterKillCounterManager : Singleton<MonsterKillCounterManager>
 {
-    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI totalMonsterCounterText;
+    
+    [SerializeField] private TextMeshProUGUI timeCountText;
+    [SerializeField] private Button resetBtn; 
+    
+    // 게임 진행, 몬스터 처치 수
+    public int TotalMonsterCounter { get; private set; }
+
+    // 절전 모드, 몬스터 처치 수
+    public int IdleModeMonsterCounter { get; private set; }
+    
     void Start()
     {
+        EnemyFSM.OnEnemyDeath += IncreaseTotalMonsterCounter; // 죽음 이벤트 구독
         
+        TotalMonsterCounter = 0; // 몬스터 카운터 초기화
+        UpdateMonsterCounterUI(); // UI 업데이트 함수 호출
+        
+        resetBtn.OnClickAsObservable().Subscribe(_ =>
+        {
+            ResetTime(); 
+        }).AddTo(this);
+    }
+    
+    void OnDestroy()
+    {
+        EnemyFSM.OnEnemyDeath -= IncreaseTotalMonsterCounter; // 이벤트 구독 해제
     }
 
-    // Update is called once per frame
-    void Update()
+    private void IncreaseTotalMonsterCounter()
+    {
+        TotalMonsterCounter++;
+        UpdateMonsterCounterUI();
+    }
+    
+    private void UpdateMonsterCounterUI()
+    {
+        totalMonsterCounterText.text = $"{TotalMonsterCounter}";
+    }
+    
+    void ResetTime()
     {
         
     }
