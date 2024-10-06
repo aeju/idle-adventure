317d190e74a958eceb6f3d92d4f115eca9816548
aeju <aejuchoe@naver.com>
2024-10-06 16:36:36 +0900

    추가: 포션 버튼 - 포션 개수 표시
    - 0 : 개수 텍스트/이미지 비활성화
    - 1개 이상 : 개수 텍스트/이미지 활성화
    - 포션 개수에 따른 UI 업데이트 처리 (PotionManager - OnPotionUpdated 이벤트)
    
    (cherry picked from commit 90196f18945444f338bd00f1d05adeaacc23fc53)

--- Assets/04. Images/1) UI/Potion/PotionBtn.cs
+++ Assets/04. Images/1) UI/Potion/PotionBtn.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PotionBtn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI potionText;
+    [SerializeField] private GameObject potionImage;
+    
+    private PotionManager potionManager;
+    
+    private void Start()
     {
-        
+        potionManager = PotionManager.Instance;
+        if (potionManager == null)
+        {
+            Debug.LogError("PotionManager instance not found!");
+            return;
+        }
+
+        // 초기 상태 설정
+        UpdatePotionDisplay();
+
+        // PotionManager의 이벤트 구독
+        potionManager.OnPotionUpdated += UpdatePotionDisplay;
+    }
+    
+    private void OnDestroy()
+    {
+        // 이벤트 구독 해제
+        if (potionManager != null)
+        {
+            potionManager.OnPotionUpdated -= UpdatePotionDisplay;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdatePotionDisplay()
     {
+        int potionCount = potionManager.current_Potion;
         
+        // 포션 개수가 1 이상일 때만 텍스트, 이미지 활성화
+        bool isActive = potionCount > 0;
+        potionText.gameObject.SetActive(isActive); // 포션 개수 
+        potionImage.SetActive(isActive); // 포션 이미지 
+
+        // 포션 개수 텍스트 업데이트
+        potionText.text = potionCount.ToString();
     }
 }
