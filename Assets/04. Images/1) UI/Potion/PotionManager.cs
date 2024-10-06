2dd6e74ce9d594313af42f65dcb0799e84e0d452
aeju <aejuchoe@naver.com>
2024-10-06 16:32:44 +0900

    추가: PotionManager - 포션 개수, 포션 이벤트, 포션 획득/사용
    - 포션 개수 : current_Potion
    - 포션 변경 시 발생 : OnPotionUpdated
    - 포션 획득 : AddPotion
    - 포션 사용 : UsePotion
    
    (cherry picked from commit c89858ba89cecdea9b09ff8aba3cc08b183a9e03)

--- Assets/04. Images/1) UI/Potion/PotionManager.cs
+++ Assets/04. Images/1) UI/Potion/PotionManager.cs
@@ -1,18 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PotionManager : MonoBehaviour
+public class PotionManager : Singleton<PotionManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public int current_Potion = 0;
+    
+    public event Action OnPotionUpdated;
+    
+    public void AddPotion(int potion)
     {
-        
+        // 추가로 획득한 코인을 추가
+        current_Potion += potion;
+        OnPotionUpdated?.Invoke();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void UsePotion(int potion)
     {
-        
+        if (current_Potion >= potion)
+        {
+            current_Potion -= potion; // 포션 감소
+            OnPotionUpdated?.Invoke(); // 포션 변화에 대한 이벤트 트리거
+        }
     }
 }
