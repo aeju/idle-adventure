d3df40fca738089cef459748ff39f721e907695d
aeju <aejuchoe@naver.com>
2024-10-06 16:29:35 +0900

    리팩토링: 리소스매니저 - 제네릭 Singleton 상속
    - 기존 : Instance 프로퍼티, Awake를 사용해 싱글톤 직접 구현
    
    (cherry picked from commit db128e618b4d739ea3957d61cc4f6b2d13d2f3ce)

--- Assets/02. Scripts/Manager/ResourceManager.cs
+++ Assets/02. Scripts/Manager/ResourceManager.cs
@@ -3,28 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceManager : MonoBehaviour 
+public class ResourceManager : Singleton<ResourceManager> 
 {
-    public static ResourceManager Instance { get; private set; }
-
     public event Action OnResourcesUpdated;
     
     public int current_Ruby = 0;
     public int current_Coin = 0;
     public int current_summon_Ticket = 0;
-    
-    void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
     
     public void AddCoin(int coin)
     {
