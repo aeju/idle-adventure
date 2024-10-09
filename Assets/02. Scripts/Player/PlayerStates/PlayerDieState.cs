1277b7696f98747a1b83f6da12d70375c7086e5a
aeju <aejuchoe@naver.com>
2024-10-09 23:15:38 +0900

    [final]추가: 플레이어 - 죽음 후, 부활
    commit fd28d24c1dc713bdda28f9831740b5c14c3949ee
    Author: aeju <aejuchoe@naver.com>
    Date:   Wed Oct 9 23:15:18 2024 +0900
    
        추가: 플레이어 - 죽음 후, 부활

--- Assets/02. Scripts/Player/PlayerStates/PlayerDieState.cs
+++ Assets/02. Scripts/Player/PlayerStates/PlayerDieState.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerDieState : IPlayerState
 {
     private PlayerController _playerController;
-
+    
+    [Header("# 플레이어 죽음 ")] 
+    private float deathTime;
+    public float REVIVE_DELAY = 2;
+    private Coroutine _reviveCoroutine;
+    
     public void Enter(PlayerController playerController)
     {
         if (!_playerController)
@@ -13,6 +18,11 @@ public class PlayerDieState : IPlayerState
         
         _playerController.anim.SetTrigger("isDead"); // 애니메이션 -> 죽음
         _playerController.isAlive = false;
+        
+        deathTime = Time.time;
+
+        // 부활 코루틴 시작
+        _reviveCoroutine = _playerController.StartCoroutine(ReviveAfterDelay());
     }
     
     public void Handle(PlayerController playerController)
@@ -23,5 +33,20 @@ public class PlayerDieState : IPlayerState
     public void Exit(PlayerController playerController)
     {
         
+    }
+    
+    private IEnumerator ReviveAfterDelay()
+    {
+        yield return new WaitForSeconds(REVIVE_DELAY);
+        
+        // 부활 로직
+        _playerController.isAlive = true;
+        _playerController.playerStats.CurrentHP = _playerController.playerStats.maxHP;
+        
+        // 애니메이션 초기화 
+        _playerController.anim.ResetTrigger("isDead");
+        
+        // Idle 상태로 전환
+        _playerController.IdlePlayer();
     }
 }
